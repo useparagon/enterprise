dbda59206f8b095b4f47d531f00500c4de19cffd
Ted O'Connor <ted.oconnor@useparagon.com>
2025-01-15 14:50:26 -0500

    feat(infra): added multiple redis instances

--- azure/workspaces/infra/redis/network.tf
+++ azure/workspaces/infra/redis/network.tf
@@ -1,29 +1,30 @@
 resource "azurerm_redis_firewall_rule" "private_subnet_access" {
-  name = replace("${var.workspace}-redis-private-access", "-", "_")
+  for_each = local.redis_instances
 
-  redis_cache_name    = azurerm_redis_cache.redis.name
+  name = replace("${var.workspace}-${each.key}-redis-private-access", "-", "_")
+
+  redis_cache_name    = azurerm_redis_cache.redis[each.key].name
   resource_group_name = var.resource_group.name
-
-  start_ip = cidrhost(var.private_subnet.address_prefixes[0], 0)
-  end_ip   = cidrhost(var.private_subnet.address_prefixes[0], -1)
+  start_ip            = cidrhost(var.private_subnet.address_prefixes[0], 0)
+  end_ip              = cidrhost(var.private_subnet.address_prefixes[0], -1)
 }
 
 resource "azurerm_redis_firewall_rule" "public_subnet_access" {
-  name = replace("${var.workspace}-redis-public-access", "-", "_")
+  for_each = local.redis_instances
 
-  redis_cache_name    = azurerm_redis_cache.redis.name
+  name                = replace("${var.workspace}-${each.key}-redis-public-access", "-", "_")
+  redis_cache_name    = azurerm_redis_cache.redis[each.key].name
   resource_group_name = var.resource_group.name
-
-  start_ip = cidrhost(var.public_subnet.address_prefixes[0], 0)
-  end_ip   = cidrhost(var.public_subnet.address_prefixes[0], -1)
+  start_ip            = cidrhost(var.public_subnet.address_prefixes[0], 0)
+  end_ip              = cidrhost(var.public_subnet.address_prefixes[0], -1)
 }
 
 resource "azurerm_redis_firewall_rule" "redis_subnet_access" {
-  name = replace("${var.workspace}-redis-self-access", "-", "_")
+  for_each = local.redis_instances
 
-  redis_cache_name    = azurerm_redis_cache.redis.name
+  name                = replace("${var.workspace}-${each.key}-redis-self-access", "-", "_")
+  redis_cache_name    = azurerm_redis_cache.redis[each.key].name
   resource_group_name = var.resource_group.name
-
-  start_ip = cidrhost(var.redis_subnet.address_prefixes[0], 0)
-  end_ip   = cidrhost(var.redis_subnet.address_prefixes[0], -1)
+  start_ip            = cidrhost(var.redis_subnet.address_prefixes[0], 0)
+  end_ip              = cidrhost(var.redis_subnet.address_prefixes[0], -1)
 }
