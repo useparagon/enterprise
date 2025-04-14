4a497fbbded3153a96da4f87ccf3887919cc4641
Ted O'Connor <ted.oconnor@useparagon.com>
2025-04-14 12:09:33 -0400

    chore: bumped prepare.sh version

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of charts, must be semver and doesn't have to match Paragon appVersion
-version="2025.3.12"
+version="2025.4.14"
 provider=${1:-aws}
 
 # allow calling from other directories
