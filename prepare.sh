0058c624a78e1cb9d8927c0259d44bad39abbe84
Ted O'Connor <ted.oconnor@useparagon.com>
2025-07-07 18:48:55 -0400

    fix: added azure load balancer healthz annotation

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of charts, must be semver and doesn't have to match Paragon appVersion
-version="2025.6.27"
+version="2025.7.7"
 provider=${1:-aws}
 
 # allow calling from other directories
