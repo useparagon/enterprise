7908dbe0c3d81ea2dcf2dc487e5c0db40538ad5f
Ted O'Connor <ted.oconnor@useparagon.com>
2025-07-10 15:57:14 -0400

    fix: removed redis debugging outputs

--- azure/workspaces/infra/redis/outputs.tf
+++ azure/workspaces/infra/redis/outputs.tf
@@ -13,23 +13,3 @@ output "redis" {
   }
   sensitive = true
 }
-
-output "redis_debugging" {
-  description = "Redis cache instances"
-  value = {
-    for key, redis in azurerm_redis_cache.redis : key => {
-      id                          = redis.id
-      name                        = redis.name
-      hostname                    = redis.hostname
-      ssl_port                    = redis.ssl_port
-      non_ssl_port                = redis.port
-      primary_access_key          = redis.primary_access_key
-      secondary_access_key        = redis.secondary_access_key
-      primary_connection_string   = redis.primary_connection_string
-      secondary_connection_string = redis.secondary_connection_string
-      private_dns_name            = try(azurerm_private_dns_a_record.redis[key].name, null)
-      private_ip_address          = try(azurerm_private_endpoint.redis[key].private_service_connection[0].private_ip_address, null)
-      sku                         = local.redis_instances[key].sku
-    }
-  }
-}
--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of charts, must be semver and doesn't have to match Paragon appVersion
-version="2025.7.8"
+version="2025.7.10"
 provider=${1:-aws}
 
 # allow calling from other directories
