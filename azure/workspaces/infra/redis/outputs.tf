b3bb2f32cd6ce6dc57de1a71fae3f9183b430530
Ted O'Connor <ted.oconnor@useparagon.com>
2025-08-26 11:23:04 -0400

    fix(infra): simplified azure redis connection string logic

--- azure/workspaces/infra/redis/outputs.tf
+++ azure/workspaces/infra/redis/outputs.tf
@@ -3,12 +3,14 @@ output "redis" {
     for key, value in local.redis_instances :
     key => {
       # Use Private Endpoint DNS names for Standard SKU, regular hostname for Premium SKU
-      host              = value.sku == "Premium" ? azurerm_redis_cache.redis[key].hostname : try(trim(azurerm_private_dns_a_record.redis[key].fqdn, "."), azurerm_redis_cache.redis[key].hostname)
-      port              = azurerm_redis_cache.redis[key].non_ssl_port_enabled ? azurerm_redis_cache.redis[key].port : azurerm_redis_cache.redis[key].ssl_port
-      password          = azurerm_redis_cache.redis[key].primary_access_key
-      ssl               = !azurerm_redis_cache.redis[key].non_ssl_port_enabled
-      cluster           = value.cluster && value.sku == "Premium"
-      connection_string = ":${azurerm_redis_cache.redis[key].primary_access_key}@${value.sku == "Premium" ? azurerm_redis_cache.redis[key].hostname : try(trim(azurerm_private_dns_a_record.redis[key].fqdn, "."), azurerm_redis_cache.redis[key].hostname)}:${azurerm_redis_cache.redis[key].non_ssl_port_enabled ? azurerm_redis_cache.redis[key].port : azurerm_redis_cache.redis[key].ssl_port}"
+      host     = value.sku == "Premium" ? azurerm_redis_cache.redis[key].hostname : try(trim(azurerm_private_dns_a_record.redis[key].fqdn, "."), azurerm_redis_cache.redis[key].hostname)
+      port     = azurerm_redis_cache.redis[key].non_ssl_port_enabled ? azurerm_redis_cache.redis[key].port : azurerm_redis_cache.redis[key].ssl_port
+      password = azurerm_redis_cache.redis[key].primary_access_key
+      ssl      = !azurerm_redis_cache.redis[key].non_ssl_port_enabled
+      cluster  = value.sku == "Premium" && value.cluster
+      connection_string = (value.sku == "Premium" && azurerm_redis_cache.redis[key].non_ssl_port_enabled ?
+        "${azurerm_redis_cache.redis[key].hostname}:${azurerm_redis_cache.redis[key].port}" :
+      ":${azurerm_redis_cache.redis[key].primary_access_key}@${try(trim(azurerm_private_dns_a_record.redis[key].fqdn, "."), azurerm_redis_cache.redis[key].hostname)}:${azurerm_redis_cache.redis[key].ssl_port}")
     }
   }
   sensitive = true
