d7ea0185937bb53ef992322298a4ddcaf346952c
Franklin Garcia <franklin.garcia@useparagon.com>
2026-01-15 23:22:12 -0400

    feat: adding readonly to the cluster to hoop_paragon_admin

--- aws/workspaces/infra/hoop/rbac.tf
+++ aws/workspaces/infra/hoop/rbac.tf
@@ -20,12 +20,35 @@ resource "kubernetes_cluster_role_binding" "hoop_cluster_admin" {
   depends_on = [kubernetes_service_account.hoop_cluster_admin]
 }
 
+# Readonly access to all namespaces
+resource "kubernetes_cluster_role_binding" "hoop_paragon_admin_readonly" {
+  count = var.hoop_enabled ? 1 : 0
+
+  metadata {
+    name = "hoop-paragon-admin-readonly"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "view"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.hoop_paragon_admin[0].metadata[0].name
+    namespace = var.hoop_namespace
+  }
+  depends_on = [kubernetes_service_account.hoop_paragon_admin]
+}
+
+# RoleBinding for admin access in paragon namespace
 resource "kubernetes_role_binding" "hoop_paragon_admin" {
   count = var.hoop_enabled ? 1 : 0
 
   metadata {
     name      = "hoop-paragon-admin"
-    namespace = var.hoop_namespace
+    namespace = "paragon"
   }
 
   role_ref {
