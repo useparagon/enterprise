0bd7e2e27bf597b064b43a6b1bf2182666c96ed3
Franklin Garcia <franklin.garcia@useparagon.com>
2026-02-03 10:35:37 -0400

    chore: adding hoop provider to child module

--- aws/workspaces/paragon/hoop/providers.tf
+++ aws/workspaces/paragon/hoop/providers.tf
@@ -1,17 +1,6 @@
+# Required so Terraform uses hoophq/hoop instead of default hashicorp/hoop (which does not exist).
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.12.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
     hoop = {
       source  = "hoophq/hoop"
       version = ">= 0.0.19"
--- gcp/workspaces/paragon/hoop/providers.tf
+++ gcp/workspaces/paragon/hoop/providers.tf
@@ -1,21 +1,6 @@
+# Required so Terraform uses hoophq/hoop instead of default hashicorp/hoop (which does not exist).
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.0.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.12.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
     hoop = {
       source  = "hoophq/hoop"
       version = ">= 0.0.19"
