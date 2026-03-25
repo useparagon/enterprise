f3906dbe521e1c0e3b4f9ffcb66460b16a5cc5da
Ted O'Connor <ted.oconnor@useparagon.com>
2026-03-25 17:42:56 -0400

    fix: added helm and k8s provider versions

--- aws/workspaces/infra/providers.tf
+++ aws/workspaces/infra/providers.tf
@@ -6,5 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.70"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
