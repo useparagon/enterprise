08f7d4850c6aa20952febf815704615141029822
Ted O'Connor <ted.oconnor@useparagon.com>
2026-03-25 16:58:53 -0400

    feat: moved providers to allow module usage

--- aws/workspaces/infra/providers.tf
+++ aws/workspaces/infra/providers.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  token      = var.aws_session_token
-  region     = var.aws_region
+terraform {
+  required_version = ">= 1.7.0"
 
-  default_tags {
-    tags = local.default_tags
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.70"
+    }
   }
 }
--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of charts, must be semver and doesn't have to match Paragon appVersion
-version="2026.03.19"
+version="2026.03.25"
 
 # defaults
 provider="aws"
